6c331ad2f14bf74949b66486c61d74ef927c2e17
WadeHarris <97800110+WadeHarris@users.noreply.github.com>
2022-06-02 23:32:52 +0200

    commit6

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,9 @@
 const temp = parseFloat(document.querySelector('.temp').textContent);
 const wind = document.querySelector('.wind').textContent;
-
 function fahrToCel (celcius) {
     const fahr = (celcius * 1.8) + 32
     return fahr
 };
-
 function windChill (airTemp, windSpeed) {
 
     if (typeof(windSpeed) == String){
@@ -18,14 +16,9 @@ function windChill (airTemp, windSpeed) {
     
     return computeWindChill
 }
-
-
 let computedTemp = fahrToCel(temp);
 let windChillFactor = windChill(computedTemp, wind)
-
 windChillFactor = windChillFactor.toFixed(2)
-
-
 if (computedTemp <= 50 && windChillFactor > 3.0){
     document.querySelector('.temp').textContent = `${computedTemp}`;
     document.querySelector('.degOrFahr').textContent = 'F'
@@ -37,3 +30,9 @@ if (computedTemp <= 50 && windChillFactor > 3.0){
     document.querySelector('.degOrFahr').textContent = 'C'
     document.querySelector('.windFactor').textContent = `N/A`;
  }
+
+// } else {
+//     document.querySelector('.temp').textContent = 'N/A';
+//     document.querySelector('.wind').textContent = 'N/A';
+// }
+
