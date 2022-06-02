80a6ec50b837396e803062e03d5aaae007e78f4d
WadeHarris <97800110+WadeHarris@users.noreply.github.com>
2022-06-02 23:00:14 +0200

    commit2

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,49 +1,22 @@
-const navBtn = document.querySelector("#nav-btn");
+function toggleMenu(){
+    document.getElementById('primaryNav').classList
+    .toggle('open');
+    document.getElementById('menu-btn').classList
+    .toggle('open');
+}
+const d = new Date();
+const last_updated = document.querySelector(".last_updated");
+const current_year = document.querySelector(".current_year");
+last_updated.textContent = document.lastModified;
+current_year.textContent = d.getFullYear();
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	d);
 
-const navEl = document.querySelector("nav");
-
-const dateEl = document.querySelector("#header-date");
-
-const footerEl = document.querySelector("#footer-year")
-
-const modEl = document.querySelector(".last-modification")
-
-navBtn.addEventListener("click", function(){
-
-    navEl.classList.toggle("open")
-    navBtn.classList.toggle("open")
-});
-
-const curDate = new Date()
-
-const curDay = function(){
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    return days[curDate.getDay()]}
-
-const curDayofMonth = curDate.getDate()
-
-const curMonth = function(){
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"]
-    return months[curDate.getMonth()]}
-
-const curYear = curDate.getFullYear()
-
-dateEl.innerHTML = `${curDay()}, ${curDayofMonth} ${curMonth()} ${curYear}`;
-
-footerEl.innerHTML = `&copy${curYear} Athlone Chamber`
-
-const lastModifiedDate = new Date(document.lastModified)
-
-let modYear = lastModifiedDate.getFullYear();
-
-let modDay = lastModifiedDate.getDay();
-
-let modMonth = lastModifiedDate.getMonth();
-
-let modminute = lastModifiedDate.getMinutes();
-
-let modsecond = lastModifiedDate.getSeconds()
-
-let modhour = lastModifiedDate.getHours(); 
-
-modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`
+datefield.innerText = `${fulldate}`;
+let day = d.getDay();
+let banner = document.querySelector('.banner')
+if(day == 1 || day == 2){
+    banner.classList.add("show-banner")
+}
