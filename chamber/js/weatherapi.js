fd46d59de453c8012a6ccd8c7b2970444ddb1067
WadeHarris <97800110+WadeHarris@users.noreply.github.com>
2022-07-03 03:45:07 +0200

    commit163

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,42 +1,66 @@
-// select HTML elements to edit
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('#caption');
-const currentWind = document.querySelector("#speed");
-const chill = document.getElementById('fw');
+const temp = document.querySelector('#temperature');
+const img = document.querySelector('#weather-img');
+const description = document.querySelector('#weather-h3');
+const windS = document.querySelector('#wind-speed');
+const windC = document.querySelector('#weather-N');
 
-//API URL with arguments  
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Merida&appid=fee85901d3800ad04d1c115528aaa349&units=metric';
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    tempe = data.main.temp;
-    wind = data.wind.speed * 3.6;
-    currentTemp.innerHTML = `<strong>${tempe.toFixed(1)}</strong>`;
-    currentWind.innerHTML = `<strong>${wind.toFixed(1)}</strong>`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-    chill.textContent = `${chillTemperature(tempe,wind)}`;
-  });
 
-function chillTemperature(celsius, kmph) {
-const fahrenheit = (celsius * 1.8) + 32;
-const mph = kmph * 0.62;
-let chillfactor = 35.74 + 0.6215 * fahrenheit - 35.75 * mph ** 0.16 + 0.4275 * fahrenheit * mph ** 0.16;
-chillfactor = chillfactor.toFixed(1);
-if ( fahrenheit > 50 || mph < 3) {
-    chilltempe = "N/A";
-} else {
-    chillCelcius = (5/9)*(chillfactor - 32)
-    chilltempe = `${chillCelcius.toFixed(1)} °C`;
-}
-return chilltempe
+// get the the weather data from openweathermap.org
+
+const url = 'https://api.openweathermap.org/data/2.5/forecast?q=Maracay&units=imperial&appid=7ef37c61e45f8bf0e9e8fdca462fac21';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            const list = data['list'];
+            console.log(list); //testing the weather data
+            displayWeather(list[0])
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+apiFetch()
+
+function displayWeather(weatherData) {
+    
+    // DISPLAY THE IMG AND DESCRITPION OF THE WEATHER
+    const desc = weatherData.weather[0].description;
+    img.setAttribute('src', `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`);
+    img.setAttribute('alt', `${desc} icon`);
+
+    // capitalize the first letter of each word
+    const arr = desc.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    
+    const capitalizeDesc = arr.join(" ");
+    description.innerHTML = capitalizeDesc;
 
 
-  
-  
+    // CALCULATE FARENHEIT TO CELCIUS AND DISPLAY IN THE HTML
+    const temperature = weatherData.main.temp.toFixed(0);
+    const c = (temperature - 32) * 5/9;
+    temp.innerHTML = c.toFixed(0);
+
+    
+    
+    // DISPLAY M/H 
+    let mph = weatherData.wind.speed;
+    windS.innerHTML = mph.toFixed(0);
+
+    
+    // DISPLAY WIND CHILL OR N/A IN THE SCREEN
+    if (temperature < 51 && mph > 3) {
+        let windChill = 35.74 + 0.6215 * temperature - 35.75 * mph**0.16 + 0.4275 * f * mph**0.16;
+        windC.innerHTML = windChill.toFixed(2);
+    } else {
+        windC.innerHTML = "N/A";
+    }
+}
